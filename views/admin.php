a402b97336bf9d03c23f7473b0b00907e04b14e7
hackedak <s.athulkrishnan100@gmail.com>
2020-12-22 18:51:30 +0530

    admin page frontend

---
+++ views/admin.php
@@ -0,0 +1,74 @@
+
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://kit.fontawesome.com/f71f402dff.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+     <!-- <link rel="stylesheet" href="../public/css/dashboard.css" type="text/css"> -->
+     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> -->
+     <link rel="stylesheet" href="../public/css/admin.css" type="text/css">
+    <title>Admin</title>
+</head>
+<body id="bootstrap-overrides">
+ 
+<!--Navbar -->
+<!-- <nav class="navbar navbar-expand-lg navbar navbar-light nav-color">
+  <a class="navbar-brand" href="http://localhost/views/dashboard.php">
+    <img src="../public/img/logo.png" width="40" height="50" class="d-inline-block align-center" alt="Logo" loading="lazy">
+    Librarion
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+    aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+    <ul class="navbar-nav ml-auto nav-flex-icons">
+      <li class="nav-item">
+      <a class="dropdown-item" href="./signOut.php"><i class="fa fa-sign-out fa-lg">Sign Out</i></a>
+      </li>
+    </ul>
+  </div>
+</nav> -->
+<!--/.Navbar -->
+<input type="checkbox" id="check">
+<label for="check">
+<i class="fas fa-bars" id="btn-icon"></i>
+<i class="fas fa-times" id="btn-cancel"></i>
+</label>
+<div class="sidebar">
+    <header>Admin Panel</header>
+    <ul>
+        <li><a href="#"></a>Add Book</li>
+        <li><a href="#"></a>Remove Book</li>
+        <li><a href="#"></a>Issue Book</li>
+        <li><a href="#"></a>Return Book</li>
+        <li><a href="#"></a>ABC</li>
+    </ul>
+</div>
+
+<section>
+    
+</section>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+ <script type="text/javascript" src="script.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+</body>
+</html>
