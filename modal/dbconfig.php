5a9b8ce503e16dc932902c66e6ce96642c68fadf
hackedak <s.athulkrishnan100@gmail.com>
2020-12-22 18:52:15 +0530

    dbconfig changed to localhost

--- modal/dbconfig.php
+++ modal/dbconfig.php
@@ -1,10 +1,10 @@
 <?php      
-    $host = "remotemysql.com";  
-    $user = "4oT9U3rgUl";  
-    $password = 'PXitfsXmcj';  
-    $db_name = "4oT9U3rgUl";  
-    $port = "3306";  
-    $con = mysqli_connect($host, $user, $password, $db_name, $port);  
+    $host = "localhost";  
+    $user = "root";  
+    $password = '';  
+    $db_name = "soelib";  
+      
+    $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
         die("Failed to connect with MySQL: ". mysqli_connect_error());  
     }  
