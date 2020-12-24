d3a646983b88d8c0f5254fb9600d83acd54f5002
hackedak <s.athulkrishnan100@gmail.com>
2020-12-24 18:18:44 +0530

    jquery hide and show implemented

--- public/js/adminControl.js
+++ public/js/adminControl.js
@@ -1,12 +1,12 @@
-
-function returnBook() {
-    const form = document.querySelector('.form-display')
-    const form1 = document.querySelector('.form-display1')
-    form1.classList.add('display-none')
-}
-
-function issueBook() {
-    const form = document.querySelector('.form-display')
-    const form1 = document.querySelector('.form-display1')
-    form1.classList.add('display-none')
-}
+$(document).ready(function () {
+    $('.sidebar .control-panel li:first').addClass('active');
+    $('.tab-content:not(:first)').hide();
+    $('.sidebar .control-panel li a').click(function (event) {
+        event.preventDefault();
+        var content = $(this).attr('href');
+        $(this).parent().addClass('active');
+        $(this).parent().siblings().removeClass('active');
+        $(content).show();
+        $(content).siblings('.tab-content').hide();
+    });
+});
