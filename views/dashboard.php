1e87efcc643d941b9677c831bca77ff461c7e9a7
hackedak <s.athulkrishnan100@gmail.com>
2020-12-30 02:12:07 +0530

    js file path updated

--- views/dashboard.php
+++ views/dashboard.php
@@ -46,7 +46,7 @@ if (!$_SESSION['username']) {
 </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-<script type="text/javascript" src="script.js"></script>
+<script type="text/javascript" src="../public/js/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 
--- views/returnBook.php
+++ views/returnBook.php
@@ -20,7 +20,7 @@ if (!$_SESSION['username']) {
 <section>
 <div class="image-container-flex">
     
-    <img src="../public/img/logo.png" class="logo-cusat" alt="" srcset="">
+    <img src="../public/img/logo.png" class="logo-cusat" alt="logo">
     <a href="admin.php"><i class="fa fa-home fa-2x" aria-hidden="true"></i></a>
 </div>
 
@@ -99,7 +99,7 @@ if (!$_SESSION['username']) {
 
 </body>
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
- <script type="text/javascript" src="admin.js"></script>
+ <script type="text/javascript" src="../public/js/admin.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
