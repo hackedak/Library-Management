92ca4638ae260b05654dfc3ce982a9186b234088
hackedak <s.athulkrishnan100@gmail.com>
2020-12-30 02:05:41 +0530

    title changed

--- views/user.php
+++ views/user.php
@@ -13,7 +13,7 @@ if (!$_SESSION['username']) {
     <!-- Bootstrap CSS -->
      <link rel="stylesheet" href="../public/css/dashboard.css" type="text/css">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <title>Welcome</title>
+    <title>User</title>
 </head>
 <body id="bootstrap-overrides">
 <?php require("./partials/navbar.php"); ?>
@@ -65,7 +65,7 @@ if (!$_SESSION['username']) {
 </body>
 
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-<script src="unreserveBook.js"></script>
+<script src="../public/js/unreserveBook.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 </body>
