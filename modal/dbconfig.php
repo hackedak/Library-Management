b3ed07385629b71dd935792eaacddc664750bb88
Atul <42874347+hackedak@users.noreply.github.com>
2021-01-12 21:51:54 +0530

    check 1

--- modal/dbconfig.php
+++ modal/dbconfig.php
@@ -1,10 +1,18 @@
 <?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "soelib";  
+    $host = "remotemysql.com";  
+    $user = "GRtY26B9ZG";  
+    $password = "I1EdgShbRK";  
+    $db_name = "GRtY26B9ZG"; 
+    $port = "3306"; 
       
-    $con = mysqli_connect($host, $user, $password, $db_name);  
+    $con = mysqli_connect($host, $user, $password, $db_name, $port);  
+
+//    $host = "localhost";  
+  //  $user = "root";  
+ //   $password = '';  
+  //  $db_name = "soelib";  
+      
+  //  $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
         die("Failed to connect with database");  
     }  
